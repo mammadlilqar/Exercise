20f3cefd991e3b1c79fd868c02c98efe4c8eab71
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2022-11-13 13:48:39 +0400

    ders 3 1

---
+++ JavaSECodes/src/Exercise.java
@@ -0,0 +1,38 @@
+
+public class Exercise {
+	public static void main(String[] args) {
+		byte b = 4;
+		System.out.println(b);
+		System.out.println("Yaddaşda 1 bayt yer tutur");
+		
+		short ş = 43;
+		System.out.println(ş);
+		System.out.println("Yaddaşda 2 bayt yer tutur");
+		
+		int i = 6;
+		System.out.println(i);
+		System.out.println("Yaddaşda 4 bayt yer tutur");
+		
+		long l = 773L;
+		System.out.println(l);
+		System.out.println("Yaddaşda 8 bayt yer tutur");
+		
+		char c = '9';
+		System.out.println(c);
+		 System.out.println("Yaddaşda 2 bayt yer tutur");
+		
+		boolean r = true;
+		System.out.println(r);
+		System.out.println("Yaddaşda 1 bit yer tutur");
+		
+		float f = 83.39F;
+		System.out.println(f);
+		System.out.println("Yaddaşda 4 bayt yer tutur");
+		
+		double t = 678.765;
+		System.out.println(t);
+		System.out.println("Yaddaşda 8 bayt yer tutur");
+
+	}
+
+}
---
+++ JavaSECodes/src/StringMisal.java
@@ -0,0 +1,8 @@
+
+public class StringMisal {
+	public static void main(String[] args) {
+		String name ="Ilqar";
+		String name1=
+	}
+
+}
