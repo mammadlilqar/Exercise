c683313bb04939a2e30833cc7d0cd10ac353f5eb
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2022-11-17 09:18:29 +0400

    Evİşi2

---
+++ JavaSECodes/src/VariablesPracticeHome.java
@@ -0,0 +1,17 @@
+
+public class VariablesPracticeHome {
+	public static void main(String[] args) {
+		int age=25;
+		System.out.println("age: "+age);
+		boolean hasjob=false;
+		System.out.println("hasjob: "+hasjob);
+		char studentGrade='C';
+	     System.out.println("studentGrade: "+studentGrade);
+	     double carPrice=25000;
+	     System.out.println("carPrice: "+carPrice);
+	     String employeeName="Məlik";
+	     System.out.println("employeeName: "+employeeName);
+	     
+	}
+
+}
--- JavaSECodes/src/misal5.java
+++ JavaSECodes/src/misal5.java
@@ -1,15 +1,10 @@
 
 public class misal5 {
 public static void main(String[] args) {
-	double a=2;
-	double b=7;
-	double c=3;
-	double d=b*b-4*a*c;
-	System.out.println(d);
-	double x1 =(-b+Math.sqrt(d))/(2*a);
-	double x2 =(-b-Math.sqrt(d))/(2*a);
-	System.out.println(x1);
-	System.out.println(x2);
-	
+	String a="I want",b;
+	b=a+" to go home.";
+	int c=b.length();
+	System.out.println(b);
+	System.out.println(c);
 }
 }
