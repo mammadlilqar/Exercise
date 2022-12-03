48ad3964229c7df42255f2009ed6b413448ad804
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2022-12-03 16:32:05 +0400

    Ders8MetodlarPraktika2

---
+++ JavaSECodes/src/ders8metodlar/Calculator.java
@@ -0,0 +1,9 @@
+package ders8metodlar;
+
+public class Calculator {
+double avarage(int a,int b) {
+	double c=(a+b)/2;
+	return c;
+	
+}
+}
---
+++ JavaSECodes/src/ders8metodlar/MethodsReturn.java
@@ -0,0 +1,12 @@
+package ders8metodlar;
+
+public class MethodsReturn {
+	public static void main(String[] args) {
+		Calculator c1= new Calculator();
+		double avg1=c1.avarage(4,6);
+		double avg2=c1.avarage(7,9);
+		System.out.println(avg1);
+		System.out.println(avg2);
+	}
+
+}
