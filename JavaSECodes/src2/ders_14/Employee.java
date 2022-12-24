d16cc45afd2358d732a64df56549ba9a1f03a266
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2022-12-24 12:48:20 +0400

    ders14VarislikTapsiriq

---
+++ JavaSECodes/src2/ders_14/Employee.java
@@ -0,0 +1,18 @@
+package ders_14;
+
+public class Employee extends Person{
+int salary;
+String department;
+String username;
+String password;
+void printinfo() {
+	super.printInfo();
+	System.out.println("salary:"+salary);
+	System.out.println("department:"+ department);
+	System.out.println("username:"+username);
+	System.out.println("password:"+password);
+
+
+}
+
+}
---
+++ JavaSECodes/src2/ders_14/Person.java
@@ -0,0 +1,20 @@
+package ders_14;
+
+public class Person {
+int id;
+String name;
+String surname;
+int age;
+String phone;
+String adress;
+ 
+void printInfo() {
+	System.out.println("id:"+id);
+	System.out.println("name:"+name);
+	System.out.println("surname:"+surname);
+	System.out.println("age:"+age);
+	System.out.println("phone:"+phone);
+	System.out.println("adress:"+adress);
+}
+}	
+
