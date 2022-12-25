c5efadaf1d001a93c984b2209b44a88631385fb2
yalizada <37800222+yalizada@users.noreply.github.com>
2022-12-25 12:51:23 -0800

    seliqe

--- JavaSECodes/src/Exercises/Calculator.java
+++ JavaSECodes/src/Exercises/Calculator.java
@@ -1,10 +1,11 @@
 package Exercises;
 
 public class Calculator{
+	
+public static void main(String[] args) {
 	Calculator k=new Calculator();
-	k.calculateCircleLength(c1);
-	System.out.println(c.length);
-
+	 
+}
 void calculateCircleLength(Circle c){
 	double pi=3.14;
 	c.length=2*c.radius*pi;
