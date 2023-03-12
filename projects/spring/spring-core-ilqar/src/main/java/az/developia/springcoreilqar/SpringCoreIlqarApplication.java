f0a80ac8d80250b66ab681c500f5e8062c00bac2
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2023-03-12 09:53:36 +0400

    Spring project was created

---
+++ projects/spring/spring-core-ilqar/src/main/java/az/developia/springcoreilqar/SpringCoreIlqarApplication.java
@@ -0,0 +1,13 @@
+package az.developia.springcoreilqar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreIlqarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreIlqarApplication.class, args);
+	}
+
+}
---
+++ projects/spring/spring-core-ilqar/src/test/java/az/developia/springcoreilqar/SpringCoreIlqarApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springcoreilqar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreIlqarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
