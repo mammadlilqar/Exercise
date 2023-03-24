c0c879efd9f05e927d1a930b6ab33c68be0112bd
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2023-03-24 11:51:06 +0400

    Spring MVC is created

---
+++ projects/spring/spring-mvc-ilqar/spring-mvc-ilqar/src/main/java/az/developia/springmvcilqar/SpringMvcIlqarApplication.java
@@ -0,0 +1,13 @@
+package az.developia.springmvcilqar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcIlqarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcIlqarApplication.class, args);
+	}
+
+}
---
+++ projects/spring/spring-mvc-ilqar/spring-mvc-ilqar/src/test/java/az/developia/springmvcilqar/SpringMvcIlqarApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springmvcilqar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcIlqarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
